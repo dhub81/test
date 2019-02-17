5a81fae45907f5a25bd7026c92afa1a0c1688f51
dhub81 <derekxue@gmail.com>
2019-02-17 18:39:27 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,5 +1,5 @@
 apt install python-pip --fix-missing
 pip install shadowsocks
-sed -i 's#cleanup#reset#g' /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py
-sleep 1
-nohup ssserver -k password -p 18388 &
+sed -i "s/cleanup/reset/g" /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py
+sync
+nohup ssserver -k password -p 8388 &
